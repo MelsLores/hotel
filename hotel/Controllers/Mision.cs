627a796c9a36aab48ca86b623d0f6aab6424e595
MelsLores <melslores@outlook.es>
2022-05-21 15:47:48 -0500

    Arrglo

---
+++ hotel/Controllers/Mision.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hotel.Controllers
+{
+    public class Mision : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ hotel/Controllers/THabit.cs
@@ -0,0 +1,13 @@
+﻿using hotel.Models.dbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hotel.Controllers
+{
+    public class THabit
+    {
+        public List<TipoHabitacion> tipoHabitacions { get; set; }
+    }
+}
